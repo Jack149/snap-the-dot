9d6029044ca9e23daddfbb3564ab2775b9434ce2
Jack149 <31501037+Jack149@users.noreply.github.com>
2020-12-04 00:51:06 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,33 +1,25 @@
 input.onButtonPressed(Button.A, function () {
-	
+    basic.showNumber(game.score())
 })
 input.onButtonPressed(Button.B, function () {
-    if (sprite.get(LedSpriteProperty.X) == 2) {
+    if (sprite.get(LedSpriteProperty.X) == 3) {
         game.addScore(1)
         basic.showLeds(`
-            # . # . #
-            . # . # .
-            # . # . #
-            . # . # .
-            # . # . #
+            . . . . .
+            . . . . #
+            . . . # .
+            # . # . .
+            . # . . .
             `)
-        basic.showLeds(`
-            . # . # .
-            # . # . #
-            . # . # .
-            # . # . #
-            . # . # .
-            `)
+        basic.pause(200)
     } else {
         game.removeLife(1)
-    }
-    if (true) {
-    	
     }
 })
 let sprite: game.LedSprite = null
 sprite = game.createSprite(2, 2)
-game.setLife(3)
+game.setLife(5)
+game.setScore(0)
 basic.forever(function () {
     sprite.move(1)
     sprite.ifOnEdgeBounce()
