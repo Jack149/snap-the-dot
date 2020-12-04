c88e655e2735ccda60eff08be06d7bf2667f953e
Jack149 <31501037+Jack149@users.noreply.github.com>
2020-12-04 01:03:30 -0600

    Update pxt.json, main.blocks, main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+def on_button_pressed_a():
+    basic.show_number(game.score())
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
+    if sprite.get(LedSpriteProperty.X) == 3:
+        game.add_score(1)
+        basic.show_leds("""
+            # . # . #
+            . # . # .
+            # . # . #
+            . # . # .
+            # . # . #
+            """)
+        basic.show_leds("""
+            . . . . .
+            . . . . #
+            . . . # .
+            # . # . .
+            . # . . .
+            """)
+        basic.pause(200)
+    else:
+        game.remove_life(1)
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+sprite: game.LedSprite = None
+sprite = game.create_sprite(2, 2)
+game.set_life(5)
+game.set_score(0)
+
+def on_forever():
+    sprite.move(1)
+    sprite.if_on_edge_bounce()
+    basic.pause(100)
+basic.forever(on_forever)
